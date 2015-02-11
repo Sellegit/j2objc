bc22ac16aa6464621d8d9a5853ee41b537c420b8
peter chen <pchen@sellegit.com>
2015-02-11 18:29:32 +0800

    staged before investigation

--- translator/src/main/java/com/google/devtools/j2objc/ast/TreeVisitor.java
+++ translator/src/main/java/com/google/devtools/j2objc/ast/TreeVisitor.java
@@ -29,8 +29,8 @@ public class TreeVisitor {
    */
   public void run(TreeNode node) {
     node.accept(this);
-//    ErrorUtil.warning("After running " + this.getClass().getSimpleName());
-//    ErrorUtil.warning(node.toString());
+    System.out.println("After running " + this.getClass().getSimpleName());
+    System.out.println(node.toString());
   }
 
   public boolean preVisit(TreeNode node) {
